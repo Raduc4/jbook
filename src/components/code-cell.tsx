3448e49cf090d5f7d9e415200b7f141df94a59de
Radu Cazacu <cazacuraduic@gmail.com>
2021-07-25 15:02:39 +0300

    Until 210

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from "react";
-
+import { Cell } from "../state";
 import bundle from "../bundler";
 import Preview from "../components/preview";
 import CodeEditor from "../components/code-editor";
 
 import Resizable from "./resizable";
 
-const CodeCell: React.FC = () => {
+import { useActions } from "../hooks/use-actions";
+
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState("");
   const [error, setError] = useState("");
   const [input, setInput] = useState<string>("");
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
+      const output = await bundle(cell.content);
       setCode(output.code);
       setError(output.err);
     }, 1000);
@@ -21,15 +28,15 @@ const CodeCell: React.FC = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <Resizable direction="ver">
       <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
         <Resizable direction="hor">
           <CodeEditor
-            initialValue="const a =1"
-            onChange={(value) => setInput(value)}
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
         <Preview code={code} err={error} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import ReactDOM from "react-dom";
-import CodeCell from "./components/code-cell";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
+import { Provider } from "react-redux";
+import { store } from "./state";
+import CellList from "./components/cell-list";
 
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 const App: React.FC = () => {
   return (
-    <>
-      <CodeCell />
-    </>
+    <Provider store={store}>
+      <div>
+        <CellList />
+      </div>
+    </Provider>
   );
 };
 
